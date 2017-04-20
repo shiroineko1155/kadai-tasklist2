5440755c7e2a7692e9c1fff411f98b9b657dd2f8
Ruriko Gotou <odoruneko24@gmail.com>
2017-04-20 13:50:09 +0000

    kadai

--- app/tasklist.php
+++ app/tasklist.php
@@ -13,3 +13,18 @@ class tasklist extends Model
         return $this->belongsTo(User::class);
     }
 }
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tasklist extends Model
+{
+    protected $fillable = ['content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,4 +38,44 @@
         </div>
     </div>
     @endif
+@endsection@extends('layouts.app')
+
+@section('content')
+    @if (Auth::check())
+        <?php $user = Auth::user(); ?>
+        <div class="row">
+            <aside class="col-xs-4">
+                {!! Form::open(['route' => 'tasklists.store']) !!}
+                
+                <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                 {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '5']) !!}
+                 </div>
+       
+                 <div class="form-group">
+                 {!! Form::label('content', 'タスク:') !!}
+                 {!! Form::textarea('status', old('status'), ['class' => 'form-control', 'rows' => '5']) !!}
+                 </div>
+      
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary btn-bloc']) !!}
+
+                {!! Form::close() !!}
+            </aside>
+            
+            <div class="col-xs-8">
+                
+                @if (count($tasklists) > 0)
+                   @include('tasklists.tasklists', ['tasklists' => $tasklists])
+                @endif
+            </div>
+        </div>
+    @else
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the TaskLists</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            
+        </div>
+    </div>
+    @endif
 @endsection
